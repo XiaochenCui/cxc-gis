a5a9bf2a63191588d3ed6013af93c0bce40663c6
XiaochenCui <jcnlcxc.new@gmail.com>
2017-12-27 00:04:15 +0800

    Add travis and requirements, ready to CI

--- test/test_point.py
+++ test/test_point.py
@@ -1,9 +1,21 @@
-from cxc_gis.models import Point
+from cxc_gis.models import Location, Region
 
 
-def init_points():
+def init_locations():
+    pass
 
 
+class TestLocation():
+    pass
 
-class TestPoint():
 
+class TestUtm():
+
+    def test_utm(self):
+        location = Location(latitude=51.2, longitude=7.5)
+        assert location.latitude == 51.2
+        assert location.longitude == 7.5
+        assert location.easting == 395201.3103811303
+        assert location.northing == 5673135.241182375
+        assert location.zone_number == 32
+        assert location.zone_letter == "U"
